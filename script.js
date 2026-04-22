aab8528a405ff11a839ecac3e45dcda2112ae02c
Victor Tramontine <132960730+Victortramontine@users.noreply.github.com>
2026-04-22 06:59:07 -0300

    mais texto

--- script.js
+++ script.js
@@ -17,9 +17,20 @@ acabou com que esse passar de tempo e de interesses ambos percebessem o quanto t
 o que começou com jogos nessas chamadas diarias passou para streams de Hexatombe nos finais de semana
 (incusive temos que terminar o natal macabro)`,
 
-`~~Ele~~ `,
+`Mas <s>ele</s> eu sinto que onde nós realmente nos ligamos foi naquele mundinho de minecraft(tenho saudades dele) onde parecia que cada momento passava tão rapido, estar ao lado de alguem durante uma chamada de apenas 16 horas parecia algo tão curto, dormir acordar novamente para voltar para a mesma chama e voltar a ficar duante horas conversando ouvindo nossa playlist enquanto construimos nossa casa e faziamos os preparativos pro ano novo, acho que nunca me senti tão em casa como naquele mundo com você.`,
 
-`a`
+`Mas como que eu não poderia sentir a melhor conexão possivel durante aquele mundo de minecraft, sendo que foi durante esses dias que li a coisa mais agradavel no instagram "voce realemnte gosta de mim ?" uma pergunta singela que pra ser sincero, nunca tive duvidas que minha resposta era com certeza eu gosto de você, mas quando você me mandou aquilo tive certeza que você tambem sentia algo por mim, e isso me fez tão feliz.`,
+
+`Você já me fez uma pergunta que eu nunca soube como responder corretamente, "o que nós somos" você me falou dessa pergunta quando sua mãe perguntou da gente, e o que posso falar é que não sei responder ao certo, mas que tanto quanto sua mãe perguntou ou quando o Bruno te mandou mensagem perguntando se a gente tava namorando, a unica certexa que eu tinha era que eu queria muito ter algo com você `,
+
+`Esses momentos todos ficaram muito marcados no meu coração durante nossas férias, ai foi a volta as nossas aulas, algo que eu tinha medo, pois passariamos bem menos tempo junto durante as chamadas.
+Mas bom, eu não podia estar mais errado né ? Porque apartir dai todo o dia nos começamos o dia trocando bom dia com nossas figurinhas, mandamos fotos um pro outro antes de ir pra faculdade, mandavamos muitos reels de casal um pro outro, e até nos chamamos de apelidos carinhosos e no final do dia era sempre de um ou de outro: "to com saudades". `,
+
+``,
+
+`Bom para finalizar, feliz aniversario para Lara, para a pessoa que me apaixonei sem nem mesmo ter visto o rosto, para a pessoa que so pelo jeito de ser me cativou de uma maneira que nenhuma outra conseguiu fazer, e pelo cuidado que tem, a sua atenção pelas minimas coisas, cada parte sua faz eu me encantar cada vez mais, acho que só se você se visse com meus olhos você conseguiria entender o quão incrivel é.
+Eu gosto tanto de você, que mesmo com essa carta, acho até pouco para me expressar, mas não sou muito bom com palavras.
+Bonitinha eu quero que saiba, eu te amo(sempre tive medo de te mandar isso, porque acho que é algo muito significativo e tenho medo de você não gostar, principalemtne pelo fato que nunca nos vimos pessoalmente, mas eu realmente gosto muito de você, então deixarei claro em cada palavra)`
 
     ];
 
